d3c2daa0b33a3a98910e1318cf27010690b46f3d
Nail Khairzamanov <n.khairzamanov@vk.team>
2026-05-08 19:36:45 +0300

    task6 initial commit

--- src/integrationTest/java/company/vk/edu/distrib/compute/KafkaTest.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KafkaTest.java
@@ -1,17 +1,28 @@
 package company.vk.edu.distrib.compute;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.net.http.HttpClient;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Testcontainers
 public class KafkaTest extends TestBase {
     static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
+    @Container
+    private static final ConfluentKafkaContainer KAFKA = new ConfluentKafkaContainer(
+            DockerImageName.parse("confluentinc/cp-kafka:7.7.8")
+    );
+
     @Test
     void succeed() {
-        assertNotNull(HTTP_CLIENT);
+        String bootstrapServers = KAFKA.getBootstrapServers();
+        assertNotNull(bootstrapServers);
     }
 
     @Override
