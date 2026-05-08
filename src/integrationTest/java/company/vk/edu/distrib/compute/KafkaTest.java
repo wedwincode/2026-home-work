43b3a77ab6ac7a5d207f7fb455283ee3d493913f
Nail Khairzamanov <n.khairzamanov@vk.team>
2026-05-08 17:39:21 +0300

    task6 initial commit

--- src/integrationTest/java/company/vk/edu/distrib/compute/KafkaTest.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/KafkaTest.java
@@ -4,11 +4,14 @@ import org.junit.jupiter.api.Test;
 
 import java.net.http.HttpClient;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class KafkaTest extends TestBase {
     static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
     @Test
     void succeed() {
+        assertNotNull(HTTP_CLIENT);
     }
 
     @Override
